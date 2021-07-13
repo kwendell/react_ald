d64e7ef7c26eac34ac5cba6602fdeaeeaae7b934
Kevin Duell <kdubzot9@gmail.com>
2021-07-13 14:22:54 -0700

    Evaluate changes

--- src/LayoutExplorer.js
+++ src/LayoutExplorer.js
@@ -7,19 +7,24 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 
 
+
 const LayoutExplorer  = () => {
 
 const [layouts, setLayouts] = useState([]);
+const [data,setData] = useState([]);
 
 
 useEffect(() => {
   fetch('http://localhost:8000/layouts')
       .then(response => response.json()) 
-      .then(data => {//console.log(data);
+      .then(data => {
            
             setLayouts(data)}
       );
 });
+
+
+
          
      
 
@@ -28,8 +33,9 @@ useEffect(() => {
 //);
 
  return (
+        
     <table id="LayoutTable" className="table table-bordered"  width="100%">
-   
+   {data.id}
   <thead>
     <tr key="header">
       <th>Price Type</th>
