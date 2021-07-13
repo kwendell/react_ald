6e25ce4e4f6549b12c0ec675a1e4ad3fe2c7e357
Kevin Duell <kdubzot9@gmail.com>
2021-07-13 15:23:08 -0700

    use reactstrap Table

--- src/LayoutExplorer.js
+++ src/LayoutExplorer.js
@@ -1,17 +1,14 @@
 
 import React from 'react';
 import { useEffect,useState } from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-
-
-
+import { Table } from 'reactstrap';
 
 
 
 const LayoutExplorer  = () => {
 
 const [layouts, setLayouts] = useState([]);
-const [data,setData] = useState([]);
+
 
 
 useEffect(() => {
@@ -25,17 +22,10 @@ useEffect(() => {
 
 
 
-         
-     
-
-// return (<div>  
- //  <table><tr><td>{layouts[0].tagType}</td><td>{layouts[0].priceType}</td></tr></table></div>
-//);
-
  return (
         
-    <table id="LayoutTable" className="table table-bordered"  width="100%">
-   {data.id}
+    <Table>
+  
   <thead>
     <tr key="header">
       <th>Price Type</th>
@@ -48,7 +38,7 @@ useEffect(() => {
        <tr key={layout.layout_id}><td className="td-lg" > {layout.wdt_type_id}</td><td className="td-lg" >{layout.update_date}</td></tr>
       ))}
   </tbody>
-  </table>
+  </Table>
   );
 
 }
