554ca607865da38575fba9a859fbefdb1b50bc93
Kevin Duell <kdubzot9@gmail.com>
2021-10-14 09:10:26 -0700

    Capture working version, cyclying with db results.

--- src/LayoutExplorer.js
+++ src/LayoutExplorer.js
@@ -12,27 +12,30 @@ const [layouts, setLayouts] = useState([]);
 
 
 useEffect(() => {
-  fetch('http://localhost:8000/layouts')
-      .then(response => response.json()) 
-      .then(data => {
-           
-            setLayouts(data)}
-      );
+
+
+  let url = 'https://shrouded-reef-81745.herokuapp.com/http://localhost:8080/altierre/asg/ws/apt/getTableContents?SelectQueryString=pricing_scenario,wdt_type_id%20from%20layout';
+  let username = 'asgadmin';
+  let password = 'asgAdm1n!';
+  let headers = new Headers();
+  headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
+  
+  fetch("www.amazon.com",headers).then(response => response.json()).then(data=>setLayouts(data));
+       
 });
 
 
 
  return (
         
-    <Table striped>
+  <table>
   
   <thead>
     <tr key="header">
       <th>Tag Type</th>
       <th>Last Updated</th>
       <th>Pricing</th>
-      
-    
+
     </tr>
   </thead>
   <tbody>
@@ -40,7 +43,7 @@ useEffect(() => {
        <tr key={layout.layout_id}><td className="td-lg" > {layout.wdt_tag_type_str}</td><td className="td-lg" >{layout.update_date}</td><td className="td-lg" >{layout.pricing_scenario}</td></tr>
       ))}
   </tbody>
-  </Table>
+  </table>
   );
 
 }
