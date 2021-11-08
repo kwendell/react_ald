741dac5fba68054a47ec1dcf12198355a86334d4
Kevin Duell <kdubzot9@gmail.com>
2021-11-08 08:37:12 -0800

    Pass width and height from fetch to tag rectangle div.

--- src/Design.js
+++ src/Design.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
+import { useState } from 'react';
 
 
 
 const Design =  (props) =>{
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
   const history = useHistory();
     const width_proportion='70%';
     const float_value = 'left';
@@ -28,10 +31,15 @@ const headers = new Headers();
 headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
 headers.set('mode', 'cors');
 
+
+
   fetch(url,headers).then(response => response.json()).then(data=>{
     // extract the tag type from the layout content
+    setWidth(data[0]);
+    setHeight(data[1]);
    
-   console.log(data);
+
+   
   
    });
  
@@ -49,7 +57,7 @@ headers.set('mode', 'cors');
   
     //<canvas ref="c" id = "c"></canvas>
     
-    return (<div style={designStyle}><div><canvas   style={canvasStyle} width="500" height="250"></canvas ></div></div>);
+    return (<div style={designStyle}><div><canvas   style={canvasStyle} width={width} height={height}></canvas ></div></div>);
 }
   
 
