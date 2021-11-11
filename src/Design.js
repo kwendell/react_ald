3c8b570411aaff38870520afd5201535c1298863
Kevin Duell <kdubzot9@gmail.com>
2021-11-11 11:34:45 -0800

    Some minor progress wtih useLayoutEffect().

--- src/Design.js
+++ src/Design.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import { fabric } from "fabric";
 
 
@@ -20,14 +20,29 @@ const Design =  (props) =>{
       padding: 20,
       backgroundColor: "#f1f1f1",    
       position:"relative",
-      float:float_value,
-      width: width_proportion,
+      float:"left"
+
   };
 
+  useLayoutEffect(() => {
+console.log("useLayoutEffect"+width);
+  });
+/*
   useEffect(() => {
     setCanvas(initCanvas());
+  
+
+ 
   }, []);
 
+  const initCanvas = () => (
+    
+    new fabric.Canvas('screen', {height:400,width:400})
+ );
+
+ */
+
+ 
   const url = `/altierre/asg/ws/apt/getTagDimension?tagTypeStr=`+history.location.tagType.tagType;
   const username = 'asgadmin';
   const password = 'asgAdm1n!';
@@ -35,45 +50,41 @@ const Design =  (props) =>{
   headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
   headers.set('mode', 'cors');
 
+ 
+
   fetch(url,headers).then(response => response.json()).then(data=>{
     // extract the tag type from the layout content
     setWidth(data[0]);
     setHeight(data[1]);  
+
    });
 
 
    /*
+    *  Obtain JSON layout from layout identifier 
     *http://localhost:8080/altierre/asg/ws/apt/getLayoutInfo?layoutIdentifierStr=555736136694090254
     */
    const layoutUrl = `/altierre/asg/ws/apt/getLayoutInfo?layoutIdentifierStr=`+history.location.layout_identifier.layout_identifier;
 
    fetch(layoutUrl,headers).then(response => response.json()).then(data=>{
    
-  //  console.log(data.pricingScenario);
-   // console.log(data.tagType);
-   // console.log(data.screens[0].fields[1].x+", "+data.screens[0].fields[1].y+", "+data.screens[0].fields[1].name+", "+data.screens[0].fields[1].width+", "+data.screens[0].fields[1].height);
-  //  console.log(data.screens[0].fields[0]);
-
-      for (let i=0; i<data.screens[0].fields.length; i++)  {
-       // console.log(data.screens[0].fields[i].name);
-       console.log(canvas);
-      }
+   
+   
    });
 
-   const initCanvas = () => (
-    new fabric.Canvas('screen', {})
- );
+   
   
   
   
    var canvasStyle = {
     border: '6px solid white', 
     borderRadius:8,
+    
   };
 
   
     
-    return (<div style={designStyle}><Mnemonics/><div><canvas id="screen"style={canvasStyle} width={width} height={height}></canvas ></div></div>);
+    return (<div style={designStyle}><Mnemonics/><canvas id="screen"style={canvasStyle} width={width} height={height}></canvas ></div>);
 }
   
 
